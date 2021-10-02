a30fc5fa4ac8a2b518f06418147148d34adeb88a
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-10-02 20:01:23 +0100

    adds conditons to catch object and array

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,29 @@
-function assertEquals(expect, actual) {}
+const compareArray = (expectedArr, actual) => {
+  if (!Array.isArray(expected)) {
+    throw new Error(`No array returned`);
+  }
+};
 
-module.exports = assertEquals
+const compareObject = (expectedObj, actual) => {
+  if (typeof actual !== "object" || Array.isArray(actual) || actual === null) {
+    throw new Errow(`No object returned`);
+  }
+};
+
+function assertEquals(expect, actual) {
+  if (typeof expect !== typeof actual) {
+    throw new Error(`${expect} and ${actual} are not the same data type`);
+  }
+
+  if (Array.isArray(expect)) {
+    return compareArray(expect, actual);
+  }
+
+  if (typeof expect === "object" && !Array.isArray(expect) && expect !== null) {
+    return compareObject(expect, actual);
+  }
+
+  return true;
+}
+
+module.exports = assertEquals;
